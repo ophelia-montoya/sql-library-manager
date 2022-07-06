3005c1860b5cd6feba0f2ededff7d3649319569e
ophelia-montoya <101679169+ophelia-montoya@users.noreply.github.com>
2022-07-05 20:57:24 -0700

    updating files

--- app.js
+++ app.js
@@ -9,19 +9,20 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Imports instance of sequelize instantiated in models/index.js
 var { sequelize } = require('./models');
 
+// Asynchronously connects to database, and tests connection
 (async () => {
+  await sequelize.sync({force:false});
   try {
     await sequelize.authenticate();
     console.log('Connection to the database established successfully!');
-
-    await sequelize.sync({force:false});
-
   } catch (error) {
     console.error('Error connecting to the database', error);
   }
 })();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -36,19 +37,43 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use( (req, res, next) => {
+  // next(createError(404, "Sorry, that page doesn't exist! Please check the URL." ));
+  const err = new Error();
+  err.status = 404;
+  err.message = "Sorry, that page doesn't exist! Please check the URL.";
+  console.log("Page not found. Please check URL.");
+  res.render('page-not-found', {err});
+
 });
 
+
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  err.status = err.status || 500;
+      err.message = err.message || 'Sorry, there was a problem with our server.';
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status);
+
+  if (err.status === 404) {
+    // console.log("Page not found. Please check the URL.")
+    res.render('page-not-found', { 
+      title: "Page Not Found",
+      error: err,
+     });
+  } else {
+    // console.log("Server error. Please try again.")
+    // err.message = err.message || 'Sorry, there was a problem with our server.';
+    res.render('error', { 
+      title: "Server Error",
+      error: err,
+    });
+  }
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,100 @@
 var express = require('express');
 var router = express.Router();
+const asyncHandler = require('express-async-handler');
+var createError = require('http-errors');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// Imports Book model
+var {Book} = require('../models');
+
+// GET home page.
+router.get('/', asyncHandler(async (req, res, next) => {
+  res.redirect('/books');
+}));
+
+// Show full list of books
+router.get('/books', asyncHandler(async (req, res, next) => {
+  const books = await Book.findAll();
+  res.render('index', { books, title: "Books" });
+}));
+
+// Show create new book form
+
+router.get('/books/new', asyncHandler(async(req, res, next) => {
+  res.render('new-book', {book: {}, title: "New Book" });
+}));
+
+// Post new book to database
+router.post('/books/new', asyncHandler(async(req, res, next) => {
+  let book;
+  try { 
+    book = await Book.create(req.body);
+    res.redirect('/');
+  } catch(error) {
+    if (error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      res.render('new-book', {book, title: "New Book", errors: error.errors});
+    } else {
+      throw error;
+    }
+  }
+
+}));
+
+// Show book detail form 
+
+router.get('/books/:id', asyncHandler(async(req, res, next) => {
+  const book = await Book.findByPk(req.params.id);
+  if (book) {
+    res.render('update-book', {book, title: "Update book"});
+  } else {
+    throw createError(404, "Sorry, that page doesn't exist.")
+  }
+}));
+
+// Updates book info in the database
+router.post('/books/:id', asyncHandler(async(req, res, next) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect('/books/');
+    } else {
+      throw createError(404, "Sorry, that page doesn't exist.")
+    }
+  } catch(error) {
+    if (error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render('update-book', {book, title: "Update Book", errors: error.errors});
+    } else {
+      throw error;
+    }
+  }
+}));
+
+router.post('/books/:id/delete', asyncHandler(async (req, res, next) => {
+  const book = await Book.findByPk(req.params.id);
+  if (book) {
+    await book.destroy();
+    res.redirect('/books/');
+  } else {
+    throw createError(500, "Sorry, that was a problem retrieving that.")
+  }
+  
+}));
+
+// Deletes a book
+
+
+
+
+
 
 module.exports = router;
+
+
+/* 
+TODO: 
+-FIX ERROR HANDLING
+*/
